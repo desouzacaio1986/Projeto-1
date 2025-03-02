ae720143a92033936ed99e777651715caf0eab14
desouzacaio1986 <90671321+desouzacaio1986@users.noreply.github.com>
2025-03-02 16:46:27 -0300

    index.php
    Pagina Inicial do Site
    - Adição do Topo, Menu e Rodapé e o CSS.

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Kirkland - Servidor de Minigames</title>
+<style>
+/* --- Sistema de Resete --- */
+*{
+margin:0;
+padding:0;
+}
+/* --- Padrão para o Corpo --- */
+body {
+background-color: #414343;
+background-size: 100%;
+font-family: Arial, sans-serif;
+color: #fff;
+}
+/* --- Cabeçario ---- */
+.topo{
+width:auto;
+height:auto;
+border-bottom:solid 3px #fff;    
+}
+/* --- Logo ---*/
+.logo{
+width:auto;
+margin:10px 0px;
+}
+.logo li{
+font:normal 36pt Arial;
+color:#fff;
+text-align:center;
+padding:10px 0px;
+}
+/* --- Menu --- */
+.menu{
+width: auto;
+height:60px;
+background-color:#313140;
+}
+.menu li{
+float:left;
+margin:15px 70px;
+text-decoration: none;
+list-style:none;
+}
+.menu li a{
+font: normal 18pt Arial;
+color:#fff;
+text-transform: uppercase;
+text-decoration: none;
+list-style:none;
+}
+.menu li a:hover{
+background-color:#CACAD9;
+}
+/* --- Rodapé --- */
+.rodape{
+border-top:solid 3px #fff;
+background-color:#313140;
+width: auto;
+height:60px;
+}
+.rodape li{
+font:normal 18pt Arial;
+color:#fff;
+text-align:center;
+padding:15px 0px ;
+}
+</style>
+<div class="topo">
+<ul class="logo">
+<li>Kirkland</li>
+<li>Servidor de Minigames</li>
+</ul>
+<ul class="menu">
+<li><a href="#">inicio</a></li>
+<li><a href="#">mini games</a></li>
+<li><a href="#">regras</a></li>
+<li><a href="#">staffs</a></li>
+<li><a href="#">doação</a></li>
+<li><a href="#">contato</a></li>
+<li><a href="#">login</a></li>
+</ul>
+</div>
+</head>
+<body>
+</body>
+<footer>
+<ul class="rodape">
+<li><strong>Kirkland< - Servidor de Minigames</strong> © 2025 - Todos os direitos reservados.</li>
+</ul>
+</footer>
+</html>
